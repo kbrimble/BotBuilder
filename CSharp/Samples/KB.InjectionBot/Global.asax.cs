1a7d0988968a4c2b4ac18cec925c9e0d635a7ec5
Kristian Brimble <kristian.brimble@just-eat.com>
2017-06-05 12:02:04 +0100

    Trying to get IoC to work

---
+++ CSharp/Samples/KB.InjectionBot/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace KB.InjectionBot
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            void UpdateContainer(ContainerBuilder builder)
+            {
+                builder.RegisterModule<HelloModule>();
+                builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            }
+
+            Conversation.UpdateContainer(UpdateContainer);
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(Conversation.Container);
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
---
+++ CSharp/Samples/KB.InjectionBot/IHelloService.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using KB.InjectionBot.Dialogs;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Internals.Fibers;
+
+namespace KB.InjectionBot
+{
+    public interface IHelloService
+    {
+        string GetMessage();
+    }
+
+    public class HelloService : IHelloService
+    {
+        public string GetMessage() => "Hello!";
+    }
+
+    public class HelloModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<HelloService>().Keyed<IHelloService>(FiberModule.Key_DoNotSerialize).As<IHelloService>();
+            builder.RegisterType<RootDialog>().As<IDialog<object>>();
+        }
+    }
+}
